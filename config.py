6665a847a619d01a7bf3b0ccde46e4b8bda1520e
Aeek True <aeektrue@gmail.com>
2022-12-01 15:46:54 +0300

    add DB vars

--- config.py
+++ config.py
@@ -2,11 +2,17 @@ import os
 import dotenv
 
 if os.path.exists('.env'):
-    dotenv.load_dotenv()
+	dotenv.load_dotenv()
 
 TOKEN = os.environ['TELEBOT_TOKEN']
 ADMIN_ID = int(os.environ['TELEBOT_ADMIN_ID'])
-WEBHOOK = os.environ['TELEBOT_WEBHOOK']
 
+WEBHOOK = os.environ['TELEBOT_WEBHOOK']
 WEBHOOK_URL_BASE = f'https://{WEBHOOK}'
 WEBHOOK_URL_PATH = f'/{TOKEN}/'
+RESET_WEBHOOK = int(os.environ['RESET_WEBHOOK'])
+
+DB_NAME = os.environ['DB_NAME']
+DB_HOST = os.environ['DB_HOST']
+DB_LOGIN = os.environ['DB_LOGIN']
+DB_PASSWORD = os.environ['DB_PASSWORD']
