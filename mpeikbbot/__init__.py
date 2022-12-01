cb3fb2ab32c00281cef59b537e4908eb6b82285e
Aeek True <aeektrue@gmail.com>
2022-12-01 21:33:15 +0300

    Reactions 2.0

--- mpeikbbot/__init__.py
+++ mpeikbbot/__init__.py
@@ -17,24 +17,24 @@ def inject_user_status(bot_instance, message: Message):
 
 @bot.message_handler(commands=['help', 'start'])
 def welcome(message: Message):
-	chat_id = message.chat.id
-	bot.send_message(chat_id, """Приветствуем тебя в Базе знаний МЭИ. Это проект студентов 1 курса.
-	/publish - опубликовать конспект
-	/search - искать конспекты""")
+	chat_id = message.chat.id 
+	bot.send_message(chat_id, """Привет! Перед тобой коллекция знаний НИУ МЭИ. Это проект студентов 1 курса.
+	/publish - загрузить конспект в Базу
+	/search - получить конспекты""")
 
 
 @bot.message_handler(commands=['search'])
 def suggest_search(message: Message):
 	chat_id = message.chat.id
 	update_user_status(message.from_user, UserStatus.WAIT_SEARCH)
-	bot.send_message(chat_id, """Перечисли все тэги которые тебе интересны.""")
+	bot.send_message(chat_id, """Напиши все теги нужных конспектов.""")
 
 
 @bot.message_handler(commands=['publish'])
 def suggest_publish(message: Message):
 	chat_id = message.chat.id
 	update_user_status(message.from_user, UserStatus.WAIT_CONSPECTUS)
-	bot.send_message(chat_id, """Пришли файл с конспектом или одну фотографию""")
+	bot.send_message(chat_id, """Отправь файл с конспектом или фотографию""")
 
 
 @bot.message_handler(content_types=['document', 'photo', 'video', 'audio'])
@@ -42,11 +42,14 @@ def get_document(message: Message):
 	chat_id = message.chat.id
 	if message.from_user.status == UserStatus.WAIT_CONSPECTUS:
 		bot.send_message(chat_id,
-						 f"""Ты прислал мне {message.content_type}. Молодчина.
-						Пришли мне тэги, для этого конспекта в формате <...>""")
+						 f"""Ты прислал мне {message.content_type}! Едем дальше!
+						Жду от тебя теги, для этого конспекта, например:
+						`матан 1курс лекции 1семестр pdf история etc.` """)
 		update_user_status(message.from_user, UserStatus.WAIT_TAG)
 	else:
-		bot.send_message("""И что мне с этим добром делать?""")
+		bot.send_message("""Правильно понимаю, что ты хочешь опубликовать этот конспект?
+		Тогда пришли тэги к нему)""")
+		update_user_status(message.from_user, UserStatus.WAIT_TAG)
 
 
 @bot.message_handler()
@@ -55,13 +58,14 @@ def get_message(message: Message):
 	if message.from_user.status == UserStatus.NEW:
 		bot.send_message(chat_id, f"""Воспользуйтесь командой /help чтобы получить список доступных команд""")
 	elif message.from_user.status == UserStatus.WAIT_SEARCH:
-		bot.send_message(chat_id, f"""Сейчас поищим ага-ага..""")
+		bot.send_message(chat_id, f"""Уже нашел, отправляю""")
+		#метод выдачи запроса
 		bot.send_message(chat_id, f"""Ничего не нашлось""")
 		update_user_status(message.from_user, UserStatus.NEW)
 	elif message.from_user.status == UserStatus.WAIT_CONSPECTUS:
-		bot.send_message(chat_id, f"""Здесь нет конспекта, попробуйте еще раз.""")
+		bot.send_message(chat_id, f"""Вы прислали не конспект""")
 	elif message.from_user.status == UserStatus.WAIT_TAG:
-		bot.send_message(chat_id, f"""Отлично конспект принят!""")
+		bot.send_message(chat_id, f"""Выдал базу, принимаю!""")
 		update_user_status(message.from_user, UserStatus.NEW)
 
 
